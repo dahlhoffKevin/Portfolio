38e929401f13ba57c826b4b95c6d45b4e21cbbc8
Kevin Dahlhoff <kdahlhoff3@gmail.com>
2024-12-15 17:03:06 +0100

    Updated footer message

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-gray-800 text-white py-4">
     <div class="container mx-auto text-center">
-      <p>&copy; 2024 kevindahlhohff.de | {{ $t('footer') }}<br>Made with VueJS 3.4, TailwindCSS</p>
+      <p>&copy; 2024 kevindahlhohff.de | {{ $t('footer') }}<br>Made with VueJS 3.4 & TailwindCSS by Kevin Dahlhoff</p>
     </div>
   </footer>
 </template>
